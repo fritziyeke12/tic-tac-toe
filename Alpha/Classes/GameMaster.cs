cc62ec509fbe62977f89c8d9ffedce2932fdc647
fritziyeke12 <fritziyeke@gmail.com>
2024-06-24 21:32:46 +0100

    Added code for all methods in GameMaster.cs

--- Alpha/Classes/GameMaster.cs
+++ Alpha/Classes/GameMaster.cs
@@ -8,7 +8,7 @@ namespace Alpha.Classes
 {
     internal class GameMaster
     {
-        public Game Game;
+        public Game Game = new Game();
         public bool Play;
         public string Instructions = "The usual rules for noughts and crosses, I'm sure you know them!";
 
@@ -17,19 +17,41 @@ namespace Alpha.Classes
             Play = true;
         }
 
-        public void NextTurn()
+        public void NextTurn(Player player1, Player player2)
         {
-
+            if (player1.IsTurn)
+            {
+                player1.MakeMove();
+            }
+            else if (player2.IsTurn)
+            {
+                player2.MakeMove();
+            }
         }
 
-        public string Announce()
+        public string Announce(Player player1, Player player2)
         {
-            return $"";
+            //return $"{(player1.IsTurn ? player2.Name : player1.Name)} just made a move on {(player1.IsTurn ? player2.PlayerMove : player1.PlayerMove)}";
+            if (!player1.IsTurn)
+            {
+                return $"{player2.Name} just made a move on [{player2.PlayerMove[0]},{player2.PlayerMove[1]}].\n\nYour turn {player1.Name}";
+            }
+            else /*if (!player2.IsTurn)*/
+            {
+                return $"{player1.Name} just made a move on [{player1.PlayerMove[0]},{player1.PlayerMove[1]}].\n\nYour turn {player2.Name}";
+            }
         }
 
         public void CheckVictor(Player player1, Player player2)
         {
-
+            if (player1.Victor)
+            {
+                Game.Over();
+            }
+            else if (player2.Victor)
+            {
+                Game.Over();
+            }
         }
     }
 }
