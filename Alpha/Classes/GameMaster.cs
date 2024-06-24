e691bac7e61db33e546780ced7df10c71e92cc15
fritziyeke12 <fritziyeke@gmail.com>
2024-06-24 20:41:36 +0100

    Fixed error in GameMaster.cs

--- Alpha/Classes/GameMaster.cs
+++ Alpha/Classes/GameMaster.cs
@@ -8,5 +8,28 @@ namespace Alpha.Classes
 {
     internal class GameMaster
     {
+        public Game Game;
+        public bool Play;
+        public string Instructions = "The usual rules for noughts and crosses, I'm sure you know them!";
+
+        public GameMaster()
+        {
+            Play = true;
+        }
+
+        public void NextTurn()
+        {
+
+        }
+
+        public string Announce()
+        {
+            return $"";
+        }
+
+        public void CheckVictor(Player player1, Player player2)
+        {
+
+        }
     }
 }
